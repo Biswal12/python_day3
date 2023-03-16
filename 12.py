d5bf822abdaf6a108b1cab9e7a3f0c1085a6710c
Biswal12 <biswalbasudev35@gmail.com>
2023-03-16 06:02:14 +0530

    update done

--- 12.py
+++ 12.py
@@ -8,8 +8,15 @@ result=[i for i in range(n1,n2+1)]
 print(result)
 array=[i for i in range(n1,n2+1)]
 print(array)
-result=[]
-for i in range(len(array)):
+result=[  ]
+"""for i in range(len(array)):
     for j in range(i,len(array)):
-        result.append(array[i:j+1])
+        result.append(array[i:j+1])"""
+
+result=[array[i:j+1]for i in range(len(array)) for j in range(i,len(array))]
 print(result)
+c=0
+for i in result:
+    if sum(i)%2!=0:
+        c+=1
+print(c)
